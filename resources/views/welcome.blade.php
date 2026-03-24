f7548f1f2bcf9bc1d1e121265c322f138c83ba37
isaiasgj0002 <isaiasguzman863@gmail.com>
2026-03-23 20:56:14 -0500

    Cambio de titulo e icono.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'ERP System') }}</title>
+    <title>Sistema ERP - Fábrica Marie / Rey del Centro</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="https://fabricamarie.com/wp-content/uploads/2022/11/logo-fam.png" />
 
     <!-- Vite - Carga los assets compilados -->
     @viteReactRefresh
